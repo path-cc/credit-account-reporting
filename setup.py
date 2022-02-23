6bb53e6ad2a9d466bc43e54736a5ec5085cecc12
Jason Patton <jpatton@cs.wisc.edu>
2022-02-23 16:24:18 -0600

    Use Elasticsearch 7 library
    The Elasticsearch connection syntax has changed in the latest
    version of the Elasticsearch Python library, so we'll pin to the
    latest 7.x release for now.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "elasticsearch", "dnspython", "XlsxWriter"],
+    install_requires=["click", "elasticsearch<8.0.0", "dnspython", "XlsxWriter"],
     entry_points={
         "console_scripts": [
             "cas_admin = cas_admin.cli:cli",
