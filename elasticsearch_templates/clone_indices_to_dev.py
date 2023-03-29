122af1b049196dc599c0eab734ace2dd5bbd2009
Jason Patton <jpatton@cs.wisc.edu>
2023-03-29 16:13:11 -0500

    HTCONDOR-1677 Add more development environment/index tools
    Added a new tool to remove existing dev charge indices
    and reset the usage in the dev account index back to 0,
    which is useful when testing changes to how credits are computed.
    
    Also cleaned up the other tools, including adding a warning when
    activating the dev environment to check for up to date cas_admin.
    Without a new cas_admin tool installed in your venv, scripts will
    instead target the production indices. (Ask me how I know... at
    least making [and "testing"] backups now!)

--- elasticsearch_templates/clone_indices_to_dev.py
+++ elasticsearch_templates/clone_indices_to_dev.py
@@ -1,6 +1,3 @@
-import json
-import sys
-import subprocess
 from cas_admin.connect import connect
 
 DRY_RUN = True
@@ -8,7 +5,6 @@ DRY_RUN = True
 ACCOUNT_INDEX = "cas-credit-accounts"
 
 CHARGE_TEMPLATE = "cas_daily_charge_records"
-CHARGE_INDICES = "cas-daily-charge-records-*"
 CHARGE_ALIAS = "cas-daily-charge-records"
 
 ILM_POLICY = "cas-daily-charge-records-ilm"
@@ -19,10 +15,11 @@ CLONE_PREFIX = "dev"
 def remove_existing_clones(es, alias, prefix, dry_run=False):
     target = f"{prefix}-{alias}-*"
     indices = es.indices.get_alias(index=target, allow_no_indices=True)
-    for index in indices:
-        if not dry_run:
-            es.indices.delete(index=index)
-        print(f"Removed existing index {index}")
+    indices = list(indices.keys())
+    indices.sort(reverse=True)
+    if not dry_run and len(indices) > 0:
+        es.indices.delete(index=indices, allow_no_indices=True)
+    print(f"Removed {len(indices)} existing indices matching {target}")
 
 
 def clone_and_fix_template(es, template, prefix, ilm_policy, alias, dry_run=False):
@@ -72,13 +69,22 @@ def clone_index(es, index, prefix, dry_run=False):
     print(f"Cloned index {index} to {target}")
 
 
+def add_aliases(es, indices, alias, prefix, dry_run=False):
+    target_indices = [f"{prefix}-{index}" for index in indices]
+    target_alias = f"{prefix}-{alias}"
+    if not dry_run:
+        es.indices.put_alias(index=target_indices[:-1], name=target_alias, body={"is_write_index": False})
+        es.indices.put_alias(index=target_indices[-1], name=target_alias, body={"is_write_index": True})
+    print(f"Added alias {target_alias} to cloned indices")
+
+
 def main():
     es = connect()
 
     # 1. Remove existing indices
     remove_existing_clones(es, CHARGE_ALIAS, CLONE_PREFIX, dry_run=DRY_RUN)
 
-    # 2. Reset dev index template
+    # 2. Clone index template and fix for dev
     clone_and_fix_template(
         es, CHARGE_TEMPLATE, CLONE_PREFIX, ILM_POLICY, CHARGE_ALIAS, dry_run=DRY_RUN
     )
@@ -91,6 +97,9 @@ def main():
     daily_charge_indices.sort()
     for daily_charge_index in daily_charge_indices:
         clone_index(es, daily_charge_index, CLONE_PREFIX, dry_run=DRY_RUN)
+
+    # 5. Add aliases
+    add_aliases(es, daily_charge_indices, CHARGE_ALIAS, CLONE_PREFIX, dry_run=DRY_RUN)
 
 
 if __name__ == "__main__":
---
+++ elasticsearch_templates/reset_dev_indices.py
@@ -0,0 +1,86 @@
+from cas_admin.connect import connect
+
+DRY_RUN = True
+
+ACCOUNT_INDEX = "cas-credit-accounts"
+
+CHARGE_TEMPLATE = "cas_daily_charge_records"
+CHARGE_ALIAS = "cas-daily-charge-records"
+
+ILM_POLICY = "cas-daily-charge-records-ilm"
+
+CLONE_PREFIX = "dev"
+
+
+def remove_existing_clones(es, alias, prefix, dry_run=False):
+    target = f"{prefix}-{alias}-*"
+    indices = es.indices.get_alias(index=target, allow_no_indices=True)
+    indices = list(indices.keys())
+    indices.sort(reverse=True)
+    if not dry_run and len(indices) > 0:
+        es.indices.delete(index=indices, allow_no_indices=True)
+    print(f"Removed {len(indices)} existing indices matching {target}")
+
+
+def clone_and_fix_template(es, template, prefix, ilm_policy, alias, dry_run=False):
+    target = f"{prefix}_{template}"
+    target_alias = f"{prefix}-{alias}"
+    body = es.indices.get_index_template(name=template)["index_templates"][0][
+        "index_template"
+    ]
+    body["index_patterns"] = f"{target_alias}-*"
+    body["template"]["settings"]["index"]["lifecycle"]["name"] = ilm_policy
+    body["template"]["settings"]["index"]["lifecycle"]["rollover_alias"] = target_alias
+    if not dry_run:
+        if es.indices.exists_index_template(name=target):
+            es.indices.delete_index_template(name=target)
+        es.indices.put_index_template(name=target, body=body)
+    print(f"Cloned and fixed index template {template} to {target}")
+
+
+def create_base_index(es, alias, prefix, dry_run=False):
+    target_alias = f"{prefix}-{index}"
+    target_index = f"{target_alias}-000001"
+    aliases = {
+        f"{target_alias}": {
+            "is_write_index": True
+        }
+    }
+
+    if not dry_run:
+        es.indices.create(index=target, aliases=aliases)
+    print(f"Created base index {target_index} with alias {target_alias}")
+
+
+def clear_account_usage(es, index, prefix, dry_run=False):
+    target = f"{prefix}-{index}"
+    body = {
+        "script": {
+            "inline": "ctx._source.total_charges = 0",
+            "lang": "painless",
+        }
+    }
+    if not dry_run:
+        es.update_by_query(index=target, body=body)
+    print(f"Cleared usage from index {target}")
+
+
+def main():
+    es = connect()
+
+    # 1. Remove existing indices
+    remove_existing_clones(es, CHARGE_ALIAS, CLONE_PREFIX, dry_run=DRY_RUN)
+
+    # 2. Clone index template and fix for dev
+    clone_and_fix_template(
+        es, CHARGE_TEMPLATE, CLONE_PREFIX, ILM_POLICY, CHARGE_ALIAS, dry_run=DRY_RUN
+    )
+
+    # 3. Create base index and alias
+    create_base_index(es, CHARGE_ALIAS, CLONE_PREFIX, dry_run=DRY_RUN)
+
+    # 4. Clear out existing credits
+    clear_account_usage(es, ACCOUNT_INDEX, CLONE_PREFIX, dry_run=DRY_RUN)
+
+if __name__ == "__main__":
+    main()
