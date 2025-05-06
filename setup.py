0ca1eccacecd81d2a910bfeffc6f9efdc2729da0
Jason Patton <jpatton@cs.wisc.edu>
2025-05-06 14:27:36 -0500

    Pin XlsxWriter to known working version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="2.0.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "elasticsearch<8.0.0", "dnspython", "XlsxWriter"],
+    install_requires=["click", "elasticsearch<8.0.0", "dnspython", "XlsxWriter<=3.2.2"],
     entry_points={
         "console_scripts": [
             "cas_admin = cas_admin.cli:cli",
